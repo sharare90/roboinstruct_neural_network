fde778af817da0d70503bc0f377426f95351a317
sharare <sharare.zehtabian@gmail.com>
2017-08-02 11:36:18 -0400

    addaptive learning rate

--- roboinstruct_nn.py
+++ roboinstruct_nn.py
@@ -5,7 +5,6 @@ x = tf.placeholder(tf.float32, [None, 15])
 W = tf.Variable(tf.random_normal(shape=[15, 8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 b = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 
-
 # def weight_variable(shape):
 #     initial = tf.truncated_normal(shape, stddev=0.1)
 #     return tf.Variable(initial)
@@ -18,10 +17,10 @@ b = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.floa
 
 y = tf.matmul(x, W) + b
 y_ = tf.placeholder(tf.float32, [None, 8])
-
+learning_rate = tf.placeholder(tf.float32, shape=[])
 cost = tf.reduce_sum(tf.pow(y - y_, 2)) / (2 * len(db.data))
 # cost = tf.reduce_mean(tf.square(y_ - y))
-train_step = tf.train.AdamOptimizer(0.05).minimize(cost)
+train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 saver = tf.train.Saver()
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
@@ -29,7 +28,11 @@ for _ in range(10000):
     # batch_xs, batch_ys = db.next_batch(100)
     batch_xs = db.data
     batch_ys = db.labels
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.5})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.1})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.01})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.001})
+
 save_path = saver.save(sess, "/home/sharare/PycharmProjects/roboinstruct_training/states/last/model.ckpt")
 error = cost
 print(sess.run(error, feed_dict={x: db.test_data, y_: db.test_labels}))
