f5a1b8840c271caa273df36c36841e2019166d1e
sharare <sharare.zehtabian@gmail.com>
2017-08-02 13:49:02 -0400

    add another layer

--- roboinstruct_nn.py
+++ roboinstruct_nn.py
@@ -2,8 +2,14 @@ import tensorflow as tf
 from database import db
 
 x = tf.placeholder(tf.float32, [None, 15])
-W = tf.Variable(tf.random_normal(shape=[15, 8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
-b = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+
+W1 = tf.Variable(tf.random_normal(shape=[15, 16], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+b1 = tf.Variable(tf.random_normal(shape=[16], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+y1 = 1.7159 * (tf.tanh(tf.matmul(2 * x / 3, W1) + b1))
+# y1 = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
+
+W2 = tf.Variable(tf.random_normal(shape=[16, 8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+b2 = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 
 # def weight_variable(shape):
 #     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -15,7 +21,7 @@ b = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.floa
 #     return tf.Variable(initial)
 
 
-y = tf.matmul(x, W) + b
+y = tf.matmul(y1, W2) + b2
 y_ = tf.placeholder(tf.float32, [None, 8])
 learning_rate = tf.placeholder(tf.float32, shape=[])
 # cost = tf.reduce_sum(tf.pow(y - y_, 2)) / (2 * len(db.data))
@@ -26,15 +32,15 @@ sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 batch_xs = db.data
 batch_ys = db.labels
-for _ in range(100):
+for _ in range(1000):
     # batch_xs, batch_ys = db.next_batch(100)
     # batch_xs = db.data
     # batch_ys = db.labels
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.5})
-for i in range(100, 1000):
+for i in range(1000, 2000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.1})
 
-for i in range(1000, 5000):
+for i in range(2000, 5000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.01})
 for i in range(5000, 10000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.001})
