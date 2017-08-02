9fb7a79b1e517ce0ad1f0c23d66396385a640583
sharare <sharare.zehtabian@gmail.com>
2017-08-02 12:00:42 -0400

    addaptive learning rate change fors

--- roboinstruct_nn.py
+++ roboinstruct_nn.py
@@ -18,19 +18,25 @@ b = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.floa
 y = tf.matmul(x, W) + b
 y_ = tf.placeholder(tf.float32, [None, 8])
 learning_rate = tf.placeholder(tf.float32, shape=[])
-cost = tf.reduce_sum(tf.pow(y - y_, 2)) / (2 * len(db.data))
-# cost = tf.reduce_mean(tf.square(y_ - y))
+# cost = tf.reduce_sum(tf.pow(y - y_, 2)) / (2 * len(db.data))
+cost = tf.reduce_mean(tf.square(y_ - y))
 train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 saver = tf.train.Saver()
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-for _ in range(10000):
+batch_xs = db.data
+batch_ys = db.labels
+for _ in range(100):
     # batch_xs, batch_ys = db.next_batch(100)
-    batch_xs = db.data
-    batch_ys = db.labels
+    # batch_xs = db.data
+    # batch_ys = db.labels
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.5})
+for i in range(100, 1000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.1})
+
+for i in range(1000, 5000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.01})
+for i in range(5000, 10000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, learning_rate: 0.001})
 
 save_path = saver.save(sess, "/home/sharare/PycharmProjects/roboinstruct_training/states/last/model.ckpt")
