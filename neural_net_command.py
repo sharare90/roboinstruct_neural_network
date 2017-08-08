32bf1a6ffcfce570f6a98a20a1750555d43672b8
sharare <sharare.zehtabian@gmail.com>
2017-08-08 14:48:40 -0400

    size of hidden layer changed

--- neural_net_command.py
+++ neural_net_command.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 import numpy as np
 import tensorflow as tf
-from settings import input_size_PCA, input_size, use_PCA
+from settings import input_size_PCA, input_size, use_PCA, first_hidden_layer
 from sklearn.externals import joblib
 
 
@@ -22,11 +22,11 @@ def evaluate(features):
     x = tf.placeholder(tf.float32, [None, input_size_PCA])
 
     W1 = tf.Variable(
-        tf.random_normal(shape=[input_size_PCA, 64], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
-    b1 = tf.Variable(tf.random_normal(shape=[64], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
+        tf.random_normal(shape=[input_size_PCA, first_hidden_layer], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
+    b1 = tf.Variable(tf.random_normal(shape=[first_hidden_layer], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
     y1 = 1.7159 * (tf.tanh(tf.matmul(2 * x / 3, W1) + b1))
 
-    W2 = tf.Variable(tf.random_normal(shape=[64, 8], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
+    W2 = tf.Variable(tf.random_normal(shape=[first_hidden_layer, 8], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
     b2 = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None))
 
     y = tf.matmul(y1, W2) + b2
--- roboinstruct_nn.py
+++ roboinstruct_nn.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
 from database import db
 
-from settings import input_size_PCA
+from settings import input_size_PCA, first_hidden_layer
 
 x = tf.placeholder(tf.float32, [None, input_size_PCA])
 
-W1 = tf.Variable(tf.random_normal(shape=[input_size_PCA, 64], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
-b1 = tf.Variable(tf.random_normal(shape=[64], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+W1 = tf.Variable(tf.random_normal(shape=[input_size_PCA, first_hidden_layer], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+b1 = tf.Variable(tf.random_normal(shape=[first_hidden_layer], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 y1 = 1.7159 * (tf.tanh(tf.matmul(2 * x / 3, W1) + b1))
 # y1 = tf.nn.relu(tf.matmul(x, W1) + b1)
 
-W2 = tf.Variable(tf.random_normal(shape=[64, 8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
+W2 = tf.Variable(tf.random_normal(shape=[first_hidden_layer, 8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 b2 = tf.Variable(tf.random_normal(shape=[8], mean=0.0, stddev=0.25, dtype=tf.float32, seed=None, name=None))
 
 # def weight_variable(shape):
