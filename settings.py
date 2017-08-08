05bd11e7a673ed9047d9cc0bd535ef7d76706f73
sharare <sharare.zehtabian@gmail.com>
2017-08-08 14:58:22 -0400

    with previous data (human)

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetworkforRoboinstruct/ScriptData"
+data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetworkforRoboinstruct/NewData/numpy"
 input_size_PCA = 15
 input_size = 15
 first_hidden_layer = 16
