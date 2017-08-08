1611c908bc262d4e6f8c900a26065dfffd48f0e9
sharare <sharare.zehtabian@gmail.com>
2017-08-08 15:37:30 -0400

    PCA added with 12 components

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetworkforRoboinstruct/ScriptData"
-input_size_PCA = 9
+input_size_PCA = 12
 input_size = 15
 first_hidden_layer = 16
 use_PCA = True
