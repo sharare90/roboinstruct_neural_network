7689107c380e42d43d3edad027473ae6c27ec150
sharare <sharare.zehtabian@gmail.com>
2017-08-08 16:37:09 -0400

    without PCA again

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetworkforRoboinstruct/ScriptData"
-input_size_PCA = 12
+input_size_PCA = 15
 input_size = 15
 first_hidden_layer = 16
-use_PCA = True
+use_PCA = False
