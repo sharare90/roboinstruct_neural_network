515a3cd2e7df0202985b3926d86c9e868ecab25f
sharare <sharare.zehtabian@gmail.com>
2017-08-09 10:28:02 -0400

    y1 changed to relu

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetworkforRoboinstruct/ScriptData"
 input_size_PCA = 15
 input_size = 15
-first_hidden_layer = 16
+first_hidden_layer = 64
 use_PCA = False
 load_folder_name = "./states/last"
 save_folder_name = "./states/last"
