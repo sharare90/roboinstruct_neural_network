663a52258ae825259ae35858144664413b6839dc
sharare <sharare.zehtabian@gmail.com>
2017-08-09 14:41:17 -0400

    batching again 500 batch size

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ valid_data_directory = "/home/sharare/SelfStudies/Robotics/RecurrentNeuralNetwor
 
 input_size_PCA = 15
 input_size = 15
-first_hidden_layer = 64
+first_hidden_layer = 16
 use_PCA = False
 load_folder_name = "./states/last"
 save_folder_name = "./states/last"
